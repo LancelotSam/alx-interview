7df255267ffaff10b9497f98d886b83d82aa1e0d
LancelotSam <skmburu110@gmail.com>
2024-07-21 08:19:54 -0400

    rotating a 2D matrix clckwise-reverse order of ops

---
+++ 0x07-rotate_2d_matrix/0-rotate_2d_matrix.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python3
+"""
+rotating a 2-D matrix by 90 degrees clockwise
+"""
+
+
+def rotate_2d_matrix(matrix):
+    # transpose it first, would be teh other way around if counter clockwise
+    for i in range(len(matrix)):
+        for j in range(i, len(matrix)):
+
+            # swapp mat[i][j] and mat[j][i]
+            matrix[i][j], matrix[j][i] = matrix[j][i], matrix[i][j]
+
+    # since its clockwise and not counter clockwise, this comes last
+    for i in range(len(matrix)):
+        matrix[i].reverse()
