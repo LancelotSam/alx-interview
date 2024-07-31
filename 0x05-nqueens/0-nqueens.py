f0f17b0b46615b7c7aebd5904fe52c15cd79b058
LancelotSam <skmburu110@gmail.com>
2024-07-30 20:23:39 -0400

    corrected for pycodestyle

--- 0x05-nqueens/0-nqueens.py
+++ 0x05-nqueens/0-nqueens.py
@@ -9,6 +9,7 @@ TODO:
 
 import sys
 
+
 def is_safe(board, row, col, N):
     # Check this column on upper rows
     for i in range(row):
@@ -18,7 +19,9 @@ def is_safe(board, row, col, N):
             return False
     return True
 
+
 def solve_nqueens(N):
+
     def solve(row, board):
         if row == N:
             solutions.append(board[:])
@@ -28,34 +31,37 @@ def solve_nqueens(N):
                 board[row] = col
                 solve(row + 1, board)
                 board[row] = -1
-    
+
     solutions = []
     board = [-1] * N
     solve(0, board)
     return solutions
+
 
 def print_solutions(solutions):
     for solution in solutions:
         formatted_solution = [[i, col] for i, col in enumerate(solution)]
         print(formatted_solution)
 
+
 def main():
     if len(sys.argv) != 2:
         print("Usage: nqueens N")
         sys.exit(1)
-    
+
     try:
         N = int(sys.argv[1])
     except ValueError:
         print("N must be a number")
         sys.exit(1)
-    
+
     if N < 4:
         print("N must be at least 4")
         sys.exit(1)
-    
+
     solutions = solve_nqueens(N)
     print_solutions(solutions)
+
 
 if __name__ == "__main__":
     main()
